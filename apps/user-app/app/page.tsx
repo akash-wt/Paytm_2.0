73fb1029b83e6538069cd056dbd129e6adba9082
akash patel <akash906129@gmail.com>
2024-12-17 03:11:30 +0530

    added singin nextAuth

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { signIn, signOut, useSession, SessionProvider } from "next-auth/react";
+import { Appbar } from "../../../packages/ui/src/appbar";
+
+export default function Page(): JSX.Element {
+  return (
+    <SessionProvider>
+      <Content />
+    </SessionProvider>
+  );
+}
+
+function Content(): JSX.Element {
+  const session = useSession();
+  return (
+    <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+    </div>
+  );
+}
